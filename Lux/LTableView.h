a38d3d9bc1c573568d593b56b728d98eec71ee18
Kyle Carson <kyleacarson@gmail.com>
2011-03-25 14:31:09 -0500

    Uhhh

---
+++ Lux/LTableView.h
@@ -0,0 +1,17 @@
+//
+//  LTableView.h
+//  Lux
+//
+//  Created by Kyle Carson on 3/24/11.
+//  Copyright 2011 Kyle Carson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface LTableView : NSTableView {
+@private
+    
+}
+
+@end
---
+++ Lux/LTableView.m
@@ -0,0 +1,29 @@
+//
+//  LTableView.m
+//  Lux
+//
+//  Created by Kyle Carson on 3/24/11.
+//  Copyright 2011 Kyle Carson. All rights reserved.
+//
+
+#import "LTableView.h"
+
+
+@implementation LTableView
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
