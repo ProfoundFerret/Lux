5e79ce491e49c29ed3b4edc386def36297d5795a
Adrien Bertrand <adrienbertrand@msn.com>
2011-03-25 23:34:34 +0100

    translations

---
+++ fr.lproj/LPlaylist.m
@@ -0,0 +1,236 @@
+//
+//  LPlaylist.m
+//  Lux
+//
+//  Created by Kyle Carson on 3/8/11.
+//  Copyright 2011 Kyle Carson. All rights reserved.
+//
+
+#import "LPlaylist.h"
+#import "LFile.h"
+#import "LExtension.h"
+#import "LFileController.h"
+#import "LExtension.h"
+#import "Lux.h"
+
+#define kMUSIC @"Musique"
+#define kVIDEO @"Vidéo"
+#define kSTREAMING @"Streaming"
+
+@implementation LPlaylist
+@synthesize title, needsUpdated, columns, smart, predicate, search, write, selectedIndexSet;
+- (id)init
+{
+    self = [super init];
+    if (self) {
+		needsUpdated = YES;
+		needsSearched = YES;
+		smart = NO;
+		write = YES;
+		
+		members = [[NSMutableDictionary alloc] init];
+		searchMembers = [[NSMutableDictionary alloc] init];
+		columns = [[NSArray alloc] initWithObjects:kINDEX, kTITLE, kARTIST, kALBUM, nil];
+		title = kUNTITLED_PLAYLIST;
+		search = @"";
+		oldSearch = @"";
+		predicate = @"";
+		
+		selectedIndexSet = [[NSIndexSet alloc] init];
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+	[members release];
+	[searchMembers release];
+	[columns release];
+	
+	[title release];
+	[search release];
+	[oldSearch release];
+	
+	[selectedIndexSet release];
+	
+    [super dealloc];
+}
+
+- (id) initWithCoder:(NSCoder *)aDecoder
+{
+	self = [super initWithCoder:aDecoder];
+	
+	needsUpdated = NO;
+	needsSearched = YES;
+	
+	smart = [[aDecoder decodeObjectForKey:kSMART] boolValue];
+	write = [[aDecoder decodeObjectForKey:kWRITE] boolValue];
+	
+	members = [[aDecoder decodeObjectForKey:kMEMBERS] retain];
+	columns = [[aDecoder decodeObjectForKey:kCOLUMNS] retain];
+	
+	title = [[aDecoder decodeObjectForKey:kPLAYLIST_TITLE] retain];
+	search = [[aDecoder decodeObjectForKey:kSEARCH] retain];
+	predicate = [[aDecoder decodeObjectForKey:kPREDICATE] retain];
+	
+	selectedIndexSet = [[aDecoder decodeObjectForKey:kSELECTED_INDEX_SET] retain];
+		
+	return self;
+}
+
+- (void) encodeWithCoder:(NSCoder *)aCoder
+{	
+	[aCoder encodeObject:[NSNumber numberWithInt:smart] forKey:kSMART];
+	[aCoder encodeObject:[NSNumber numberWithInt:write] forKey:kWRITE];
+	
+	[aCoder encodeObject:members forKey:kMEMBERS];
+	[aCoder encodeObject:columns forKey:kCOLUMNS];
+	
+	[aCoder encodeObject:title forKey:kPLAYLIST_TITLE];
+	[aCoder encodeObject:search forKey:kSEARCH];
+	[aCoder encodeObject:predicate forKey:kPREDICATE];
+	
+	[aCoder encodeObject:selectedIndexSet forKey:kSELECTED_INDEX_SET];
+	
+	[super encodeWithCoder:aCoder];
+}
+
+- (void) update
+{
+	@synchronized(self)
+	{
+		if (! needsUpdated || ! smart) return;
+		needsUpdated = NO;
+		needsSearched = YES;
+		
+		[members release];
+		members = [[NSMutableDictionary alloc] init];
+		NSPredicate * pred = [NSPredicate predicateWithFormat:predicate];
+		NSDictionary * fileList = [[LFileController sharedInstance] files];
+
+		for (NSString * path in fileList)
+		{
+			LFile * file = [fileList objectForKey:path];
+			BOOL shouldAdd = [pred evaluateWithObject:[file dictionary]];
+			if ( shouldAdd )
+			{
+				[members setObject:file forKey:[file url]];
+			}
+		}
+	}
+}
+
+- (NSDictionary *) allMembers
+{
+	return [NSDictionary dictionaryWithDictionary:members];
+}
+
+- (NSDictionary *) members
+{
+	[self update];
+	
+	NSDictionary * mList;
+	if ([search length])
+	{
+		[self updateSearch];
+		mList = [NSDictionary dictionaryWithDictionary:searchMembers];
+	} else {
+		mList = [NSDictionary dictionaryWithDictionary:members];
+	}
+	return mList;
+}
+
+- (void) updateSearch
+{
+	if (! needsSearched || ! [search length]) return;
+	needsSearched = NO;
+	
+	NSDictionary * toBeSearched;
+	
+	if ([search rangeOfString:oldSearch].location != NSNotFound) // If the new search contains the old one then use old results as a start
+	{
+		toBeSearched = [NSDictionary dictionaryWithDictionary:searchMembers];
+	} else {
+		toBeSearched = [NSDictionary dictionaryWithDictionary:members];
+	}
+	
+	NSArray * searchSet = [search componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+	[searchMembers release];
+	searchMembers = [[NSMutableDictionary alloc] init];
+	
+	for (NSURL * url in toBeSearched)
+	{
+		LFile * f = [toBeSearched objectForKey:url];
+		if ([f matchesSearchSet: searchSet])
+		{
+			[searchMembers setObject:f forKey:[f url]];
+		}
+	}
+}
+
+- (void) setSearch:(NSString *) aSearch
+{
+	needsSearched = YES;
+	oldSearch = [search retain];
+	search = [[aSearch lowercaseString] retain];
+	[[Lux sharedInstance] reloadData];
+}
+
++ (LPlaylist *) musicPlaylist
+{
+	LPlaylist * playlist = [[[LPlaylist alloc] init] autorelease];
+	
+	NSString * predicate = [NSString stringWithFormat:@"%@ = %d", kFILE_TYPE, LFileTypeAudio];
+	[playlist setPredicate:predicate];
+	[playlist setSmart:YES];
+	[playlist setTitle:kMUSIC];
+	
+	[playlist setWrite:NO];
+	
+	return playlist;
+}
+
++ (LPlaylist *) videoPlaylist
+{
+	LPlaylist * playlist = [[[LPlaylist alloc] init] autorelease];
+	
+	NSString * predicate = [NSString stringWithFormat:@"%@ = %d", kFILE_TYPE, LFileTypeVideo];
+	
+	[playlist setPredicate:predicate];
+	[playlist setSmart:YES];
+	[playlist setTitle:kVIDEO];
+	
+	NSArray * columns = [NSArray arrayWithObjects:kINDEX, kTITLE, nil];
+	[playlist setColumns:columns];
+	
+	[playlist setWrite:NO];
+	
+	return playlist;
+}
+
++ (LPlaylist *) streamingPlaylist
+{
+	LPlaylist * playlist = [[[LPlaylist alloc] init] autorelease];
+	
+	NSString * predicate = [NSString stringWithFormat:@"%@ = %d", kFILE_TYPE, LFileTypeStreaming];
+
+	[playlist setPredicate:predicate];
+	[playlist setSmart:YES];
+	[playlist setTitle:kSTREAMING];
+	
+	NSArray * columns = [NSArray arrayWithObjects:kINDEX, kTITLE, nil];
+	[playlist setColumns:columns];
+	
+	[playlist setWrite:NO];
+	
+	return playlist;
+}
+
+- (void) setTitle: (NSString *) newTitle
+{
+	if (! write) return;
+	
+	title = newTitle;
+}
+@end
---
+++ fr.lproj/LPlaylistController.h
@@ -0,0 +1,40 @@
+//
+//  LPlaylistController.h
+//  Lux
+//
+//  Created by Kyle Carson on 3/9/11.
+//  Copyright 2011 Kyle Carson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "LControllerObject.h"
+#import "LPlaylist.h"
+
+#define kPLAYLISTS @"Listes de lecture"
+#define kLIBRARY @"Bibliothèque"
+#define kACTIVE_PLAYLIST @"activePlaylist"
+#define kVISIBLE_PLAYLIST @"visiblePlaylist"
+
+@interface LPlaylistController : LControllerObject {
+    NSMutableDictionary * playlists;
+	LPlaylist * activePlaylist;
+	LPlaylist * visiblePlaylist;
+}
+
+- (void) setupLibraryPlaylists;
+
+- (void) addPlaylist: (LPlaylist *) playlist;
+- (void) addPlaylist: (LPlaylist *) playlist toGroupNamed:(NSString *) name;
+
+- (NSMutableArray *) getPlaylists;
+- (NSMutableArray *) getPlaylistsFromGroup: (NSString *) name;
+- (NSMutableArray *) allPlaylists;
+
+- (void) prepPlaylistsForUpdate;
+
+- (void) searchChangedTo: (NSString *) search;
+
+@property (readwrite, assign) NSMutableDictionary * playlists;
+@property (readwrite, assign, nonatomic) LPlaylist * activePlaylist;
+@property (readwrite, assign, nonatomic) LPlaylist * visiblePlaylist;
+@end
