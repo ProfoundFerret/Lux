dd85ee4c4e86d65f1dc3ef70b7c9c81dc62f1ad0
Kyle Carson <kyleacarson@gmail.com>
2011-03-31 12:35:10 -0500

    Fix NSStringCategory file

---
+++ NSStringCategory.h
@@ -0,0 +1,13 @@
+//
+//  NSStringCategory.h
+//  Lux
+//
+//  Created by Kyle Carson on 3/31/11.
+//  Copyright 2011 ProfoundFerret. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSString (NSStringCategory)
+@end
---
+++ NSStringCategory.m
@@ -0,0 +1,14 @@
+//
+//  NSStringCategory.m
+//  Lux
+//
+//  Created by Kyle Carson on 3/31/11.
+//  Copyright 2011 ProfoundFerret. All rights reserved.
+//
+
+#import "NSStringCategory.h"
+
+
+@implementation NSString (NSStringCategory)
+
+@end
