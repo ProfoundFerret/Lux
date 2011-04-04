76ba21f0d5a809b61ecdd7bec4df309a6eacf00b
Kyle Carson <elyknosrac@gmail.com>
2011-04-04 16:13:54 -0500

    Oops =)

---
+++ Lux/LLocalization.h
@@ -0,0 +1,36 @@
+//
+//  LLocalization.h
+//  Lux
+//
+//  Created by Kyle Carson on 4/4/11.
+//  Copyright 2011 ProfoundFerret. All rights reserved.
+//
+
+#define kPLAY_TEXT @"Play"
+#define kPAUSE_TEXT @"Pause"
+#define kNEXT_TEXT @"Next"
+#define kPREVIOUS_TEXT @"Previous"
+#define kPLAY_RECENT_TEXT @"Play Recent"
+#define kREPEAT_TEXT @"Repeat"
+#define kSHUFFLE_TEXT @"Shuffle"
+#define kPAUSED_TEXT @"(Paused)"
+#define kSHOW_IN_FINDER_TEXT @"Show In Finder"
+#define kSHOW_IN_PLAYLIST @"Show In Playlist"
+#define kADD_TO_PLAYLIST_TEXT @"Add To Playlist"
+#define kNEW_PLAYLIST @"New Playlist"
+#define kDELETE_FROM_TEXT @"Delete From"
+#define kDELETE_FROM_LIBRARY_TEXT @"Delete From Library"
+
+#define kDUPLICATE_TEXT @"Duplicate"
+#define kDELETE_TEXT @"Delete"
+#define kCONVERT_TO_REGULAR_PLAYLIST_TEXT @"Convert to Regular Playlist"
+#define kRENAME_TEXT @"Rename"
+
+#define kMUSIC_FOLDER @"Music"
+#define kVIDEO_TEXT @"Video"
+#define kSTREAMING_TEXT @"Streaming"
+
+#define kPLAYLISTS_TEXT @"Playlists"
+#define kLIBRARY_TEXT @"Library"
+
+#define kUNTITLED_PLAYLIST_TEXT @"Untitled Playlist"
